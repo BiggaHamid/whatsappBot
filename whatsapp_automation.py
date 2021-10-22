596f46501c302fbb8fe68d85b5b72d9bee326e0f
BiggaHamid <90086463+BiggaHamid@users.noreply.github.com>
2021-10-22 13:26:51 +0300

    Add files via upload

---
+++ whatsapp_automation.py
@@ -0,0 +1,31 @@
+try:
+    import pywhatkit
+    import termcolor
+except:
+    import os
+    
+    os.system("pip3 install pywhatkit")
+    os.system("pip3 install termcolor")
+    
+import pywhatkit
+import termcolor
+    
+class Autobot:
+    def __init__(self, contact, message, hour, minute):
+        self.contact = contact
+        self.message = message
+        self.hour = hour
+        self.minute = minute
+        
+    def send_message(self):
+        result = pywhatkit.sendwhatmsg(self.contact, self.message, self.hour, self.minute)
+        return result
+        
+contact=input(termcolor.colored("Enter Target's Contact: ","cyan"))
+message=input(termcolor.colored("Enter Message: ","cyan"))
+hour=input(termcolor.colored("Enter Hour to send message: ","cyan"))
+minute=input(termcolor.colored("Enter Minute to send message: ","cyan"))
+
+data = (contact, message, hour, minute)
+
+Autobot.send_message(data)
